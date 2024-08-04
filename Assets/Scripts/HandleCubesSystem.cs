8e7144801cae3e967f3aad2701438df05d2ae191
EsbenNyboeKanda <enj@kanda.dk>
2024-08-04 15:29:27 +0200

    Add aspect example

--- Assets/Scripts/HandleCubesSystem.cs
+++ Assets/Scripts/HandleCubesSystem.cs
@@ -1,16 +1,18 @@
 using Unity.Entities;
-using Unity.Transforms;
 
 public partial struct HandleCubesSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (localTransform, rotateSpeed, movement)
-                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>, RefRO<Movement>> ()
-                     .WithAll<RotatingCube>())
+        // foreach (var (localTransform, rotateSpeed, movement)
+        //          in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>, RefRO<Movement>> ()
+        //              .WithAll<RotatingCube>())
+        // localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime);
+        // localTransform.ValueRW = localTransform.ValueRO.Translate(movement.ValueRO.MovementVector * SystemAPI.Time.DeltaTime);
+        foreach (var rotatingMovingCubeAspect
+                 in SystemAPI.Query<RotatingMovingCubeAspect>())
         {
-            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime);
-            localTransform.ValueRW = localTransform.ValueRO.Translate(movement.ValueRO.MovementVector * SystemAPI.Time.DeltaTime);
+            rotatingMovingCubeAspect.MoveAndRotate(SystemAPI.Time.DeltaTime);
         }
     }
 }
