64ab117473fb8c656883cf74c385f7f8fc8ce987
EsbenNyboeKanda <enj@kanda.dk>
2024-08-11 15:53:34 +0200

    Replace grid-stuff with ECS pathfinding tutorial (NOT FINISHED)

---
+++ Assets/DOTS_Pathfinding/Scripts/PathFollow.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PathFollowAuthoring : MonoBehaviour
+{
+    public class Baker : Baker<PathFollowAuthoring>
+    {
+        public override void Bake(PathFollowAuthoring authoring)
+        {
+            var entity = GetEntity(authoring);
+            AddComponent(entity, new PathFollow());
+        }
+    }
+}
+
+public struct PathFollow : IComponentData
+{
+    public int pathIndex;
+}
---
+++ Assets/_/PathfindingOld/PrefabEntityComponent.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PrefabEntityComponentAuthoring : MonoBehaviour
+{
+    public class Baker : Baker<PrefabEntityComponentAuthoring>
+    {
+        public override void Bake(PrefabEntityComponentAuthoring authoring)
+        {
+            var entity = GetEntity(authoring);
+            AddComponent(entity, new PrefabEntityComponent());
+        }
+    }
+}
+
+public struct PrefabEntityComponent : IComponentData
+{
+    public Entity prefabEntity;
+}
