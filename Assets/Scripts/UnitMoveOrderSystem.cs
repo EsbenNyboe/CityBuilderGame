192169d8ef14646e44ee41428899590c1f643bb6
EsbenNyboeKanda <enj@kanda.dk>
2024-08-16 09:18:46 +0200

    Make mouse-position into path-target

--- Assets/Scripts/UnitMoveOrderSystem.cs
+++ Assets/Scripts/UnitMoveOrderSystem.cs
@@ -1,3 +1,4 @@
+using CodeMonkey.Utils;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -7,20 +8,39 @@ public partial class UnitMoveOrderSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!Input.GetMouseButtonDown(0))
         {
-            var entityCommandBuffer = new EntityCommandBuffer(WorldUpdateAllocator);
-
-            foreach (var (localTransform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithEntityAccess())
-            {
-                entityCommandBuffer.AddComponent(entity, new PathfindingParams
-                {
-                    StartPosition = new int2(0, 0),
-                    EndPosition = new int2(4, 0)
-                });
-            }
-
-            entityCommandBuffer.Playback(EntityManager);
+            return;
         }
+
+        var mousePosition = UtilsClass.GetMouseWorldPosition();
+        var cellSize = PathfindingGridSetup.Instance.pathfindingGrid.GetCellSize();
+
+        PathfindingGridSetup.Instance.pathfindingGrid.GetXY(mousePosition + new Vector3(1, 1) * cellSize * 0.5f, out var endX, out var endY);
+
+        ValidateGridPosition(ref endX, ref endY);
+
+        var entityCommandBuffer = new EntityCommandBuffer(WorldUpdateAllocator);
+
+        foreach (var (localTransform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithEntityAccess())
+        {
+            PathfindingGridSetup.Instance.pathfindingGrid.GetXY(localTransform.ValueRO.Position, out var startX, out var startY);
+
+            ValidateGridPosition(ref startX, ref startY);
+
+            entityCommandBuffer.AddComponent(entity, new PathfindingParams
+            {
+                StartPosition = new int2(startX, startY),
+                EndPosition = new int2(endX, endY)
+            });
+        }
+
+        entityCommandBuffer.Playback(EntityManager);
+    }
+
+    private void ValidateGridPosition(ref int x, ref int y)
+    {
+        x = math.clamp(x, 0, PathfindingGridSetup.Instance.pathfindingGrid.GetWidth() - 1);
+        y = math.clamp(y, 0, PathfindingGridSetup.Instance.pathfindingGrid.GetHeight() - 1);
     }
 }
