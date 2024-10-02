d2d5508967f53b107b7b5324d703ec645d630a58
Esben <esbennj@hotmail.com>
2024-10-02 20:34:31 +0200

    Add camera-movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private void Update()
+    {
+        var moveDelta = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDelta.x -= _movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDelta.x += _movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDelta.y -= _movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDelta.y += _movementSpeed * Time.deltaTime;
+        }
+
+        transform.position += moveDelta;
+    }
+}
