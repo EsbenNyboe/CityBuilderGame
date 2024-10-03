026d5c4d31b71d61a96bc4d6d1c91ff6f9b7d04a
Esben <esbennj@hotmail.com>
2024-10-03 10:20:02 +0200

    Delete selected units on press-delete

--- Assets/Scripts/UnitControl/SelectedUnitSystem.cs
+++ Assets/Scripts/UnitControl/SelectedUnitSystem.cs
@@ -15,5 +15,22 @@ public partial class SelectedUnitSystem : SystemBase
         {
             Graphics.DrawMesh(mesh, localTransform.ValueRO.Position + positionOffset, Quaternion.identity, material, 0);
         }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            DeleteSelectedUnits();
+        }
+    }
+
+    private void DeleteSelectedUnits()
+    {
+        var entityCommandBuffer = new EntityCommandBuffer(WorldUpdateAllocator);
+
+        foreach (var (unitSelection, entity) in SystemAPI.Query<RefRO<UnitSelection>>().WithEntityAccess())
+        {
+            entityCommandBuffer.DestroyEntity(entity);
+        }
+
+        entityCommandBuffer.Playback(EntityManager);
     }
 }
