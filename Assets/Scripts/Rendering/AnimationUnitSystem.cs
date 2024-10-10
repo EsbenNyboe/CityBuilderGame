da1b333c0a1d792ae5b4b56a6c26d7ac1a2df3c6
Esben <esbennj@hotmail.com>
2024-10-10 21:31:08 +0200

    Optimize animation-state system with Burst

--- Assets/Scripts/Rendering/AnimationUnitSystem.cs
+++ Assets/Scripts/Rendering/AnimationUnitSystem.cs
@@ -1,9 +1,11 @@
+using Unity.Burst;
 using Unity.Entities;
 using UnityEngine;
 
-public partial class AnimationUnitSystem : SystemBase
+public partial struct AnimationUnitSystem : ISystem
 {
-    protected override void OnUpdate()
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
     {
         // var entityCommandBuffer = new EntityCommandBuffer(WorldUpdateAllocator);
         foreach (var (spriteSheetAnimation, animationUnitIdle, pathFollow, entity) in SystemAPI
@@ -12,9 +14,9 @@ public partial class AnimationUnitSystem : SystemBase
         {
             if (pathFollow.ValueRO.PathIndex < 0)
             {
-                EntityManager.SetComponentEnabled<AnimationUnitWalk>(entity, false);
+                state.EntityManager.SetComponentEnabled<AnimationUnitWalk>(entity, false);
 
-                EntityManager.SetComponentEnabled<AnimationUnitIdle>(entity, true);
+                state.EntityManager.SetComponentEnabled<AnimationUnitIdle>(entity, true);
                 spriteSheetAnimation.ValueRW.FrameCount = animationUnitIdle.ValueRO.FrameCount;
                 spriteSheetAnimation.ValueRW.FrameTimerMax = animationUnitIdle.ValueRO.FrameTimerMax;
                 spriteSheetAnimation.ValueRW.Uv = new Vector4(1f / animationUnitIdle.ValueRO.FrameCount, 0.5f, 0, animationUnitIdle.ValueRO.FrameRow);
@@ -27,9 +29,9 @@ public partial class AnimationUnitSystem : SystemBase
         {
             if (pathFollow.ValueRO.PathIndex >= 0)
             {
-                EntityManager.SetComponentEnabled<AnimationUnitIdle>(entity, false);
+                state.EntityManager.SetComponentEnabled<AnimationUnitIdle>(entity, false);
 
-                EntityManager.SetComponentEnabled<AnimationUnitWalk>(entity, true);
+                state.EntityManager.SetComponentEnabled<AnimationUnitWalk>(entity, true);
                 spriteSheetAnimation.ValueRW.FrameCount = animationUnitWalk.ValueRO.FrameCount;
                 spriteSheetAnimation.ValueRW.FrameTimerMax = animationUnitWalk.ValueRO.FrameTimerMax;
                 spriteSheetAnimation.ValueRW.Uv = new Vector4(1f / animationUnitWalk.ValueRO.FrameCount, 0.5f, 0, animationUnitWalk.ValueRO.FrameRow);
