37de4f972ad220e423a8727aa773815ffe036e0d
Esben <esbennj@hotmail.com>
2024-10-14 21:23:40 +0200

    Fix race-condition error: Set idle animation as default

--- Assets/Scripts/Rendering/SpriteSheetAnimationAuthoring.cs
+++ Assets/Scripts/Rendering/SpriteSheetAnimationAuthoring.cs
@@ -1,23 +1,51 @@
+using System;
 using Unity.Entities;
 using UnityEngine;
 
 public class SpriteSheetAnimationAuthoring : MonoBehaviour
 {
-    // [SerializeField] private int _frameCount = 4;
-    //
-    // [SerializeField] private float _frameTimerMax = 0.1f;
+    [SerializeField] private AnimationConfig _idleAnimationConfig;
+    [SerializeField] private AnimationConfig _walkAnimationConfig;
+
+    [Serializable]
+    public class AnimationConfig
+    {
+        public int FrameCount;
+        public float FrameRow;
+        public float FrameTimerMax;
+    }
 
     public class SpriteSheetAnimationDataBaker : Baker<SpriteSheetAnimationAuthoring>
     {
         public override void Bake(SpriteSheetAnimationAuthoring authoring)
         {
             var entity = GetEntity(TransformUsageFlags.Dynamic);
-            AddComponent(entity,
-                new SpriteSheetAnimation
-                {
-                    // FrameCount = authoring._frameCount,
-                    // FrameTimerMax = authoring._frameTimerMax
-                });
+
+            // Idle animation config
+            AddComponent(entity, new AnimationUnitIdle
+            {
+                FrameCount = authoring._idleAnimationConfig.FrameCount,
+                FrameRow = authoring._idleAnimationConfig.FrameRow,
+                FrameTimerMax = authoring._idleAnimationConfig.FrameTimerMax
+            });
+            SetComponentEnabled<AnimationUnitIdle>(entity, false);
+
+            // Walk animation config
+            AddComponent(entity, new AnimationUnitWalk
+            {
+                FrameCount = authoring._walkAnimationConfig.FrameCount,
+                FrameRow = authoring._walkAnimationConfig.FrameRow,
+                FrameTimerMax = authoring._walkAnimationConfig.FrameTimerMax
+            });
+            SetComponentEnabled<AnimationUnitWalk>(entity, false);
+
+            // Animation component: Set idle animation as default
+            AddComponent(entity, new SpriteSheetAnimation
+            {
+                FrameCount = authoring._idleAnimationConfig.FrameCount,
+                FrameTimerMax = authoring._idleAnimationConfig.FrameTimerMax,
+                Uv = new Vector4(1f / authoring._idleAnimationConfig.FrameCount, 0.5f, 0, authoring._idleAnimationConfig.FrameRow)
+            });
         }
     }
 }
@@ -30,4 +58,18 @@ public struct SpriteSheetAnimation : IComponentData
     public float FrameTimerMax;
     public Vector4 Uv;
     public Matrix4x4 Matrix;
+}
+
+public struct AnimationUnitIdle : IComponentData, IEnableableComponent
+{
+    public int FrameCount;
+    public float FrameRow;
+    public float FrameTimerMax;
+}
+
+public struct AnimationUnitWalk : IComponentData, IEnableableComponent
+{
+    public int FrameCount;
+    public float FrameRow;
+    public float FrameTimerMax;
 }
