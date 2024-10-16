e3b93114f60aec8796ffe65dea29a297aa378c59
Esben <esbennj@hotmail.com>
2024-10-16 21:05:56 +0200

    Cleanup: Remove Singleton-code

--- Assets/Scripts/GridUnmanaged/GridManagerSystem.cs
+++ Assets/Scripts/GridUnmanaged/GridManagerSystem.cs
@@ -32,16 +32,6 @@ public partial class GridManagerSystem : SystemBase
             grid[i] = cell;
         }
 
-        // RequireForUpdate<GridManager>();
-        // var entity = EntityManager.CreateSingleton<GridManager>();
-
-        // SystemAPI.SetComponent(entity, new GridManager
-        // {
-        //     Width = width,
-        //     Height = height,
-        //     WalkableGrid = grid
-        // });
-
         EntityManager.AddComponent<GridManager>(SystemHandle);
         SystemAPI.SetComponent(SystemHandle, new GridManager
         {
@@ -59,7 +49,6 @@ public partial class GridManagerSystem : SystemBase
             return;
         }
 
-        // var gridManager = SystemAPI.GetSingleton<GridManager>();
         var gridManager = SystemAPI.GetComponent<GridManager>(SystemHandle);
 
         for (var i = 0; i < gridManager.WalkableGrid.Length; i++)
@@ -76,62 +65,9 @@ public partial class GridManagerSystem : SystemBase
 
     protected override void OnDestroy()
     {
-        // var gridManager = SystemAPI.GetSingleton<GridManager>();
         var gridManager = SystemAPI.GetComponent<GridManager>(SystemHandle);
         Debug.Log("Test: " + gridManager.WalkableGrid[0].IsWalkable);
         Debug.Log("Test2: " + gridManager.WalkableGridIsDirty);
         gridManager.WalkableGrid.Dispose();
     }
 }
-
-// public partial struct GridUnmanagedSystem : ISystem
-// {
-//     public void OnCreate(ref SystemState state)
-//     {
-//         state.RequireForUpdate<GridManager>();
-//         var entity = state.EntityManager.CreateSingleton<GridManager>();
-//         state.EntityManager.AddComponent<GridManager>(new SystemHandle());
-//
-//         var width = 100;
-//         var height = 200;
-//
-//         var grid = new NativeArray<WalkableCell>(width * height, Allocator.Persistent);
-//         for (var i = 0; i < grid.Length; i++)
-//         {
-//             var cell = grid[i];
-//             cell.IsWalkable = true;
-//             grid[i] = cell;
-//         }
-//
-//         SystemAPI.SetComponent(entity, new GridManager
-//         {
-//             Width = width,
-//             Height = height,
-//             WalkableGrid = grid
-//         });
-//     }
-//
-//     public void OnUpdate(ref SystemState state)
-//     {
-//         if (!Input.GetKeyDown(KeyCode.Return))
-//         {
-//             return;
-//         }
-//
-//         var gridUnmanaged = SystemAPI.GetSingleton<GridManager>();
-//
-//         for (var i = 0; i < gridUnmanaged.WalkableGrid.Length; i++)
-//         {
-//             var cell = gridUnmanaged.WalkableGrid[i];
-//             cell.IsWalkable = cell.IsWalkable;
-//             cell.IsDirty = true;
-//             gridUnmanaged.WalkableGrid[i] = cell;
-//         }
-//     }
-//
-//     public void OnDestroy(ref SystemState state)
-//     {
-//         var gridUnmanaged = SystemAPI.GetSingleton<GridManager>();
-//         gridUnmanaged.WalkableGrid.Dispose();
-//     }
-// }
