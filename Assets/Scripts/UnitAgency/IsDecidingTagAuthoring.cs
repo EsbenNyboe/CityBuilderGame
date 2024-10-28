22aa09f2f677a06c17b4ee301012b934a2fa1160
Mads <mbh@kanda.dk>
2024-10-28 22:56:56 +0100

    Work in progress on UnitAgency stuff

---
+++ Assets/Scripts/UnitAgency/IsDecidingTagAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace UnitAgency
+{
+	public class IsDecidingTagAuthoring : MonoBehaviour
+	{
+		public class IsDecidingTagBaker : Baker<IsDecidingTagAuthoring>
+		{
+			public override void Bake(IsDecidingTagAuthoring authoring)
+			{
+				var entity = GetEntity(TransformUsageFlags.None);
+				AddComponent<IsDecidingTag>(entity);
+			}
+		}
+	}
+}
