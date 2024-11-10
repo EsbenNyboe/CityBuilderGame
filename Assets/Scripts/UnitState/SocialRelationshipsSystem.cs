b9386e4da5d57d558cf2a09f627df9c4658cb1e6
Mads <mbh@kanda.dk>
2024-11-10 15:09:09 +0100

    WIP

---
+++ Assets/Scripts/UnitState/SocialRelationshipsSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace UnitState
+{
+    public struct SocialRelationships : IComponentData
+    {
+        public NativeHashMap<Entity, float> Relationships;
+    }
+    
+    [UpdateInGroup(typeof(SpawningSystemGroup))]
+    [UpdateAfter(typeof(SpawnManagerSystem))]
+    [UpdateAfter(typeof(SpawnUnitsSystem))]
+    public partial struct SocialRelationshipsSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            using var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
+            ecb.Playback(state.EntityManager);
+        }
+    }
+}
