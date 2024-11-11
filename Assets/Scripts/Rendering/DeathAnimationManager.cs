4c905a12136b368cf8deeca5f06311521b31d81f
EsbenNyboeKanda <enj@kanda.dk>
2024-11-11 17:27:30 +0100

    Add object-pooling to death-animation

--- Assets/Scripts/Rendering/DeathAnimationManager.cs
+++ Assets/Scripts/Rendering/DeathAnimationManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Rendering
@@ -7,15 +8,37 @@ namespace Rendering
         public static DeathAnimationManager Instance;
         [SerializeField] private GameObject _deathAnimationPrefab;
 
+        private Queue<ParticleSystem> _pool;
+
         private void Awake()
         {
             Instance = this;
+
+            _pool = new Queue<ParticleSystem>();
+            var newPoolItem = CreatePoolItem();
+            _pool.Enqueue(newPoolItem);
+        }
+
+        private ParticleSystem CreatePoolItem()
+        {
+            return Instantiate(_deathAnimationPrefab, Vector3.zero, Quaternion.identity).GetComponent<ParticleSystem>();
         }
 
         public void PlayDeathAnimation(Vector3 position)
         {
-            var effect = Instantiate(_deathAnimationPrefab, position, Quaternion.identity);
-            Destroy(effect, 1);
+            ParticleSystem poolItem;
+            if (_pool.TryPeek(out var nextItem) && !nextItem.isPlaying)
+            {
+                poolItem = _pool.Dequeue();
+            }
+            else
+            {
+                poolItem = CreatePoolItem();
+            }
+
+            poolItem.transform.position = position;
+            poolItem.Play();
+            _pool.Enqueue(poolItem);
         }
     }
 }
