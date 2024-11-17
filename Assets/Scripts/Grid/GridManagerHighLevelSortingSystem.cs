8921ce4cd8e7e31bf261813a82bc93f897620bc3
Esben <esbennj@hotmail.com>
2024-11-17 18:03:29 +0100

    Code cleanup

--- Assets/Scripts/Grid/GridManagerHighLevelSortingSystem.cs
+++ Assets/Scripts/Grid/GridManagerHighLevelSortingSystem.cs
@@ -17,17 +17,13 @@ namespace Grid
         private NativeList<int2> _debugList;
         private NativeList<int2> _debugListSuccessHits;
         private NativeHashMap<int2, int> _debugSectionMap;
-        private bool _isDebugging;
         private bool _hasQuickDebuggedWholeSection;
         private int _currentSectionBeingDebugged;
-        private bool _initialized;
-
 
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<DebugToggleManager>();
             _gridManagerSystemHandle = state.World.GetExistingSystem(typeof(GridManagerSystem));
-            _isDebugging = true;
         }
 
         [BurstCompile]
@@ -36,92 +32,11 @@ namespace Grid
             SortingProcess(ref state);
         }
 
-        private void DebugLogic(ref SystemState state)
+        public void OnDestroy(ref SystemState state)
         {
-            if (Input.GetKeyDown(KeyCode.U))
-            {
-                _isDebugging = !_isDebugging;
-            }
-
-            if (!_isDebugging)
-            {
-                return;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                _hasQuickDebuggedWholeSection = false;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Return) ||
-                (Input.GetKey(KeyCode.Return) && Input.GetKey(KeyCode.LeftShift) && !_hasQuickDebuggedWholeSection))
-            {
-                if (_debugSectionMap.TryGetValue(_debugList[_debugListCurrent],
-                        out var currentSection))
-                {
-                    if (_currentSectionBeingDebugged != currentSection)
-                    {
-                        _currentSectionBeingDebugged = currentSection;
-                        _hasQuickDebuggedWholeSection = true;
-                        return;
-                    }
-                }
-
-                if (_debugListSuccessHitsCurrent == 0)
-                {
-                    if (_debugListCurrent == 0)
-                    {
-                        _debugListSuccessHitsCurrent++;
-                        if (_debugListSuccessHitsCurrent >= _debugListSuccessHits.Length)
-                        {
-                            _debugListSuccessHitsCurrent = 0;
-                        }
-                    }
-                }
-                else if (_debugListSuccessHits[_debugListSuccessHitsCurrent].Equals(_debugList[_debugListCurrent]))
-                {
-                    _debugListSuccessHitsCurrent++;
-                    if (_debugListSuccessHitsCurrent >= _debugListSuccessHits.Length)
-                    {
-                        _debugListSuccessHitsCurrent = 0;
-                    }
-                }
-
-                _debugListCurrent++;
-                if (_debugListCurrent >= _debugList.Length)
-                {
-                    _debugListCurrent = 0;
-                }
-
-                // Debug.Log(_debugList[_debugListCurrent]);
-            }
-
-
-            var color = Color.red;
-            if (_debugListCurrent == 0)
-            {
-                color = Color.blue;
-            }
-            else if (_debugListSuccessHitsCurrent != 0 && _debugListSuccessHits[_debugListSuccessHitsCurrent]
-                         .Equals(_debugList[_debugListCurrent]))
-            {
-                color = Color.green;
-            }
-
-            DebugDrawCell(_debugList[_debugListCurrent], color);
-        }
-
-        private static void DebugDrawCell(int2 cell, Color color)
-        {
-            var padding = 0.2f;
-            var offset = 1f - padding;
-            var debugPosition = new Vector3(cell.x - 0.5f + padding, cell.y - 0.5f + padding, 0);
-            Debug.DrawLine(debugPosition, debugPosition + new Vector3(+offset, +0), color);
-            Debug.DrawLine(debugPosition, debugPosition + new Vector3(+0, +offset), color);
-            Debug.DrawLine(debugPosition + new Vector3(+offset, +0), debugPosition + new Vector3(+offset, +offset),
-                color);
-            Debug.DrawLine(debugPosition + new Vector3(+0, +offset), debugPosition + new Vector3(+offset, +offset),
-                color);
+            _debugList.Dispose();
+            _debugListSuccessHits.Dispose();
+            _debugSectionMap.Dispose();
         }
 
         private void SortingProcess(ref SystemState state)
@@ -169,7 +84,7 @@ namespace Grid
             var closedNodes = new NativeParallelHashMap<int2, WalkableSectionNode>(walkablesCount, Allocator.Temp);
             SearchCellList(walkableSections, openNodes, closedNodes, gridManager.NeighbourDeltas, isDebug);
 
-            if (isDebug && _isDebugging)
+            if (isDebug)
             {
                 using var sectionKeys = walkableSections.GetKeyArray(Allocator.Temp);
                 var sectionCount = 0;
@@ -180,11 +95,6 @@ namespace Grid
                         sectionCount = key + 1;
                     }
                 }
-
-                // if (Input.GetKeyDown(KeyCode.P))
-                // {
-                //     Debug.Log("Section count: " + sectionCount);
-                // }
 
                 var sectionKey = 0;
                 while (sectionKey < sectionCount)
@@ -211,39 +121,6 @@ namespace Grid
             closedNodes.Dispose();
         }
 
-        private Color GetSectionColor(int sectionKey)
-        {
-            var random = new Random((uint)(sectionKey * 999 + 1));
-            var color = new Color
-            {
-                r = random.NextFloat(),
-                g = random.NextFloat(),
-                b = random.NextFloat(),
-                a = 1
-            };
-            return color;
-
-            var colorIndex = sectionKey % 7;
-            return colorIndex switch
-            {
-                0 => Color.cyan,
-                1 => Color.magenta,
-                2 => Color.yellow,
-                3 => Color.blue,
-                4 => Color.black,
-                5 => Color.white,
-                6 => Color.green,
-                _ => Color.gray
-            };
-        }
-
-        public void OnDestroy(ref SystemState state)
-        {
-            _debugList.Dispose();
-            _debugListSuccessHits.Dispose();
-            _debugSectionMap.Dispose();
-        }
-
         private void SearchCellList(NativeParallelMultiHashMap<int, WalkableSectionNode> walkableSections,
             NativeParallelHashSet<int2> openNodes, NativeParallelHashMap<int2, WalkableSectionNode> closedNodes,
             NativeArray<int2> neighbours, bool isDebug)
@@ -267,10 +144,7 @@ namespace Grid
                 {
                     _debugList.Add(currentCell);
                     _debugListSuccessHits.Add(currentCell);
-                    if (!_debugSectionMap.TryAdd(currentCell, currentSection))
-                    {
-                        Debug.Log("Try add failed (1)");
-                    }
+                    _debugSectionMap.Add(currentCell, currentSection);
                 }
 
                 while (currentNode.NeighboursSearched < neighbours.Length)
@@ -295,10 +169,7 @@ namespace Grid
                         if (isDebug)
                         {
                             _debugListSuccessHits.Add(neighbourCell);
-                            if (!_debugSectionMap.TryAdd(neighbourCell, currentSection))
-                            {
-                                Debug.Log("Try add failed (2)");
-                            }
+                            _debugSectionMap.Add(neighbourCell, currentSection);
                         }
                     }
 
@@ -317,5 +188,98 @@ namespace Grid
             public int2 NodeSource;
             public int NeighboursSearched;
         }
+
+        #region Debugging
+
+        private void DebugLogic(ref SystemState state)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                _hasQuickDebuggedWholeSection = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return) ||
+                (Input.GetKey(KeyCode.Return) && Input.GetKey(KeyCode.LeftShift) && !_hasQuickDebuggedWholeSection))
+            {
+                if (_debugSectionMap.TryGetValue(_debugList[_debugListCurrent],
+                        out var currentSection))
+                {
+                    if (_currentSectionBeingDebugged != currentSection)
+                    {
+                        _currentSectionBeingDebugged = currentSection;
+                        _hasQuickDebuggedWholeSection = true;
+                        return;
+                    }
+                }
+
+                if (_debugListSuccessHitsCurrent == 0)
+                {
+                    if (_debugListCurrent == 0)
+                    {
+                        _debugListSuccessHitsCurrent++;
+                        if (_debugListSuccessHitsCurrent >= _debugListSuccessHits.Length)
+                        {
+                            _debugListSuccessHitsCurrent = 0;
+                        }
+                    }
+                }
+                else if (_debugListSuccessHits[_debugListSuccessHitsCurrent].Equals(_debugList[_debugListCurrent]))
+                {
+                    _debugListSuccessHitsCurrent++;
+                    if (_debugListSuccessHitsCurrent >= _debugListSuccessHits.Length)
+                    {
+                        _debugListSuccessHitsCurrent = 0;
+                    }
+                }
+
+                _debugListCurrent++;
+                if (_debugListCurrent >= _debugList.Length)
+                {
+                    _debugListCurrent = 0;
+                }
+            }
+
+
+            var color = Color.red;
+            if (_debugListCurrent == 0)
+            {
+                color = Color.blue;
+            }
+            else if (_debugListSuccessHitsCurrent != 0 && _debugListSuccessHits[_debugListSuccessHitsCurrent]
+                         .Equals(_debugList[_debugListCurrent]))
+            {
+                color = Color.green;
+            }
+
+            DebugDrawCell(_debugList[_debugListCurrent], color);
+        }
+
+        private static void DebugDrawCell(int2 cell, Color color)
+        {
+            var padding = 0.2f;
+            var offset = 1f - padding;
+            var debugPosition = new Vector3(cell.x - 0.5f + padding, cell.y - 0.5f + padding, 0);
+            Debug.DrawLine(debugPosition, debugPosition + new Vector3(+offset, +0), color);
+            Debug.DrawLine(debugPosition, debugPosition + new Vector3(+0, +offset), color);
+            Debug.DrawLine(debugPosition + new Vector3(+offset, +0), debugPosition + new Vector3(+offset, +offset),
+                color);
+            Debug.DrawLine(debugPosition + new Vector3(+0, +offset), debugPosition + new Vector3(+offset, +offset),
+                color);
+        }
+
+        private Color GetSectionColor(int sectionKey)
+        {
+            var random = new Random((uint)(sectionKey * 999 + 1));
+            var color = new Color
+            {
+                r = random.NextFloat(),
+                g = random.NextFloat(),
+                b = random.NextFloat(),
+                a = 1
+            };
+            return color;
+        }
+
+        #endregion
     }
 }
