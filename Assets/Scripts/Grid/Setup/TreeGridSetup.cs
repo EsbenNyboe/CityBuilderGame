e97f70d8a7e37f78c25912c62fde73926eb92240
EsbenNyboeKanda <enj@kanda.dk>
2024-11-20 12:40:20 +0100

    Cleanup: Move tree-setup scripts

---
+++ Assets/Scripts/Grid/Setup/TreeGridSetup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Grid.Setup
+{
+    public class TreeGridSetup : MonoBehaviour
+    {
+        public static TreeGridSetup Instance;
+        [SerializeField] private AreaToExclude[] _areasToExclude;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public static AreaToExclude[] AreasToExclude()
+        {
+            return Instance._areasToExclude;
+        }
+
+        [Serializable]
+        public class AreaToExclude
+        {
+            public int2 StartCell;
+            public int2 EndCell;
+        }
+    }
+}
