27c0d592fe1f574a64496f4844d2d58877dcff09
EsbenNyboeKanda <enj@kanda.dk>
2024-11-21 16:51:17 +0100

    Randomize punch-area

--- Assets/Scripts/Effects/SpriteEffectManager.cs
+++ Assets/Scripts/Effects/SpriteEffectManager.cs
@@ -5,6 +5,9 @@ public class SpriteEffectManager : MonoBehaviour
     public static SpriteEffectManager Instance;
 
     [SerializeField] private TimedImagePoolManager[] _damageEffectPools;
+
+    [Range(0, 0.5f)] [SerializeField] private float _randomPositionFactor;
+
     private int _currentSelection;
 
     private void Awake()
@@ -21,6 +24,8 @@ public class SpriteEffectManager : MonoBehaviour
         }
 
         var poolItem = _damageEffectPools[_currentSelection].GetOrCreatePoolItem();
+        position.x += Random.Range(-_randomPositionFactor, _randomPositionFactor);
+        position.y += Random.Range(-_randomPositionFactor, _randomPositionFactor);
         _damageEffectPools[_currentSelection].EnqueuePoolItem(poolItem, position);
     }
 }
