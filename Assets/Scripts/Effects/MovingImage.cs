6368eeecf35476d9fe958b004dbcc1385cbbd6b2
EsbenNyboeKanda <enj@kanda.dk>
2024-11-23 23:16:04 +0100

    Fix social-effect transforms

--- Assets/Scripts/Effects/MovingImage.cs
+++ Assets/Scripts/Effects/MovingImage.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class MovingImage : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _offset;
 
     private void Update()
     {
         var position = transform.position;
         position.y += _moveSpeed * Time.deltaTime;
         transform.position = position;
+    }
+
+    private void OnEnable()
+    {
+        transform.position += new Vector3(0, _offset, 0);
     }
 }
