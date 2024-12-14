54dfd6a4aff68d15c0628eecc946f82f5aea56cb
Esben <esbennj@hotmail.com>
2024-12-14 21:44:35 +0100

    Decouple Effects from Statistics

---
+++ Assets/Scripts/Statistics/SocialEffectsCounterSystem.cs
@@ -0,0 +1,30 @@
+using Effects.SocialEffectsRendering;
+using Unity.Entities;
+
+namespace Statistics
+{
+    public partial class SocialEffectsCounterSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var numOfPositiveEffects = 0;
+            var numOfNegativeEffects = 0;
+
+            foreach (var (socialEffect, entity) in SystemAPI.Query<RefRO<SocialEffect>>().WithAll<SocialEffect>()
+                         .WithEntityAccess())
+            {
+                if (socialEffect.ValueRO.Type == SocialEffectType.Positive)
+                {
+                    numOfPositiveEffects++;
+                }
+                else
+                {
+                    numOfNegativeEffects++;
+                }
+            }
+
+            UnitStatsDisplayManager.Instance.SetNumberOfPositiveSocialEffects(numOfPositiveEffects);
+            UnitStatsDisplayManager.Instance.SetNumberOfNegativeSocialEffects(numOfNegativeEffects);
+        }
+    }
+}
