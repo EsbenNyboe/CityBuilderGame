e3504583b4de43b418d17a7ea2550669b92b3785
Esben <esbennj@hotmail.com>
2024-12-14 15:19:35 +0100

    Import structure-types as part of world-sprite-sheet

--- Assets/Scripts/Rendering/WorldSpriteSheetConfig.cs
+++ Assets/Scripts/Rendering/WorldSpriteSheetConfig.cs
@@ -56,6 +56,10 @@ namespace Rendering
         BoarRun,
         SpearHolding,
         SpearThrowing,
-        Spear
+        Spear,
+        DropPoint,
+        TreeDamaged,
+        Tree,
+        Bed
     }
 }
