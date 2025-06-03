f0510c6d838833f3b539ff4fd8ee6db9c1b802a7
EsbenNyboeKanda <enj@kanda.dk>
2025-06-03 21:55:20 +0200

    Setup baby-spawning WIP

--- Assets/Scripts/Rendering/AnimationSelection/UnitAnimationSelectionSystem.cs
+++ Assets/Scripts/Rendering/AnimationSelection/UnitAnimationSelectionSystem.cs
@@ -3,7 +3,6 @@ using Rendering;
 using SystemGroups;
 using UnitBehaviours.Pathing;
 using UnitBehaviours.Sleeping;
-using UnitBehaviours.Tags;
 using UnitBehaviours.Talking;
 using UnitBehaviours.Targeting;
 using Unity.Burst;
@@ -18,6 +17,7 @@ namespace UnitBehaviours.UnitConfigurators
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            // Boar
             foreach (var (unitAnimationSelection, pathFollow) in SystemAPI.Query<RefRW<UnitAnimationSelection>, RefRO<PathFollow>>().WithAll<Boar>())
             {
                 unitAnimationSelection.ValueRW.SelectedAnimation = pathFollow.ValueRO.IsMoving()
@@ -25,21 +25,39 @@ namespace UnitBehaviours.UnitConfigurators
                     : WorldSpriteSheetEntryType.BoarStand;
             }
 
-            foreach (var (unitAnimationSelection, _) in
-                     SystemAPI.Query<RefRW<UnitAnimationSelection>, RefRO<IsThrowingSpear>>().WithAll<IsHoldingSpear>())
-            {
-                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.SpearHolding;
-            }
-
-            foreach (var (unitAnimationSelection, _) in SystemAPI.Query<RefRW<UnitAnimationSelection>, RefRO<IsThrowingSpear>>()
-                         .WithNone<IsHoldingSpear>())
-            {
-                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.SpearThrowing;
-            }
-
+            // Baby Villager
             foreach (var (unitAnimationSelection, pathFollow, inventory) in SystemAPI
                          .Query<RefRW<UnitAnimationSelection>, RefRO<PathFollow>, RefRO<InventoryState>>()
-                         .WithNone<IsSleeping>().WithNone<IsTalking>().WithNone<IsThrowingSpear>().WithAll<Villager>())
+                         .WithNone<IsSleeping>().WithNone<IsTalking>().WithNone<IsThrowingSpear>().WithAll<Villager>().WithAll<Baby>())
+            {
+                var hasItem = inventory.ValueRO.CurrentItem != InventoryItem.None;
+                if (pathFollow.ValueRO.IsMoving())
+                {
+                    unitAnimationSelection.ValueRW.SelectedAnimation =
+                        hasItem ? WorldSpriteSheetEntryType.WalkHolding : WorldSpriteSheetEntryType.BabyWalk;
+                }
+                else
+                {
+                    unitAnimationSelection.ValueRW.SelectedAnimation =
+                        hasItem ? WorldSpriteSheetEntryType.IdleHolding : WorldSpriteSheetEntryType.BabyIdle;
+                }
+            }
+
+            foreach (var unitAnimationSelection in SystemAPI.Query<RefRW<UnitAnimationSelection>>()
+                         .WithPresent<IsSleeping>().WithAll<Baby>())
+            {
+                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.BabySleep;
+            }
+
+            foreach (var unitAnimationSelection in SystemAPI.Query<RefRW<UnitAnimationSelection>>().WithAll<IsTalking>().WithAll<Baby>())
+            {
+                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.BabyWalk;
+            }
+
+            // Villager
+            foreach (var (unitAnimationSelection, pathFollow, inventory) in SystemAPI
+                         .Query<RefRW<UnitAnimationSelection>, RefRO<PathFollow>, RefRO<InventoryState>>()
+                         .WithNone<IsSleeping>().WithNone<IsTalking>().WithNone<IsThrowingSpear>().WithAll<Villager>().WithNone<Baby>())
             {
                 var hasItem = inventory.ValueRO.CurrentItem != InventoryItem.None;
                 if (pathFollow.ValueRO.IsMoving())
@@ -55,14 +73,26 @@ namespace UnitBehaviours.UnitConfigurators
             }
 
             foreach (var unitAnimationSelection in SystemAPI.Query<RefRW<UnitAnimationSelection>>()
-                         .WithPresent<IsSleeping>())
+                         .WithPresent<IsSleeping>().WithNone<Baby>())
             {
                 unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.Sleep;
             }
 
-            foreach (var unitAnimationSelection in SystemAPI.Query<RefRW<UnitAnimationSelection>>().WithAll<IsTalking>())
+            foreach (var unitAnimationSelection in SystemAPI.Query<RefRW<UnitAnimationSelection>>().WithAll<IsTalking>().WithNone<Baby>())
             {
                 unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.Talk;
+            }
+
+            foreach (var (unitAnimationSelection, _) in
+                     SystemAPI.Query<RefRW<UnitAnimationSelection>, RefRO<IsThrowingSpear>>().WithAll<IsHoldingSpear>())
+            {
+                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.SpearHolding;
+            }
+
+            foreach (var (unitAnimationSelection, _) in SystemAPI.Query<RefRW<UnitAnimationSelection>, RefRO<IsThrowingSpear>>()
+                         .WithNone<IsHoldingSpear>())
+            {
+                unitAnimationSelection.ValueRW.SelectedAnimation = WorldSpriteSheetEntryType.SpearThrowing;
             }
         }
     }
