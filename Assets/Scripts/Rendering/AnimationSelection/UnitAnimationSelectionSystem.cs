2ac3fb65d113b5ccb819e5b2aa383774593931b4
EsbenNyboeKanda <enj@kanda.dk>
2025-06-09 19:25:10 +0200

    Fix config

--- Assets/Scripts/Rendering/AnimationSelection/UnitAnimationSelectionSystem.cs
+++ Assets/Scripts/Rendering/AnimationSelection/UnitAnimationSelectionSystem.cs
@@ -34,11 +34,13 @@ namespace UnitBehaviours.UnitConfigurators
                 var hasItem = inventory.ValueRO.CurrentItem != InventoryItem.None;
                 if (pathFollow.ValueRO.IsMoving())
                 {
+                    // TODO: Baby animation for WalkHolding
                     unitAnimationSelection.ValueRW.SelectedAnimation =
                         hasItem ? WorldSpriteSheetEntryType.WalkHolding : WorldSpriteSheetEntryType.BabyWalk;
                 }
                 else
                 {
+                    // TODO: Baby animation for IdleHolding
                     unitAnimationSelection.ValueRW.SelectedAnimation =
                         hasItem ? WorldSpriteSheetEntryType.IdleHolding : WorldSpriteSheetEntryType.BabyIdle;
                 }
