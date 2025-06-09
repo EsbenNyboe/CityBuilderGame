d685eea2535e0cfaced344e81a82a4a37e24fe41
EsbenNyboeKanda <enj@kanda.dk>
2025-06-09 14:39:51 +0200

    Fix broken save-slots

--- Assets/Scripts/Grid/SaveLoad/SavedGridStateObject.cs
+++ Assets/Scripts/Grid/SaveLoad/SavedGridStateObject.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -7,12 +8,12 @@ namespace Grid.SaveLoad
     public class SavedGridStateObject : ScriptableObject
     {
         public int2 GridSize;
-        public int2[] Trees;
-        public int2[] Beds;
-        public int2[] Storages;
-        public int2[] Bonfires;
-        public int2[] Houses;
-        public float3[] Villagers;
-        public float3[] Boars;
+        public int2[] Trees = Array.Empty<int2>();
+        public int2[] Beds = Array.Empty<int2>();
+        public int2[] Storages = Array.Empty<int2>();
+        public int2[] Bonfires = Array.Empty<int2>();
+        public int2[] Houses = Array.Empty<int2>();
+        public float3[] Villagers = Array.Empty<float3>();
+        public float3[] Boars = Array.Empty<float3>();
     }
 }
